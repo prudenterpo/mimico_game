47ba0731d6471db2dbca6ff6c159b52acf5e0692
rodrigopdo <rodrigo.prudente82@gmail.com>
2025-03-16 11:03:08 -0300

    Feat: Add CustomTextField.tsx

---
+++ src/components/UI/CustomTextField.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+interface CustomTextFieldProps {
+    label: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+    placeholder?: string;
+    fullWidth?: boolean;
+    disabled?: boolean;
+    error?: boolean;
+    helperText?: string;
+}
+
+export const CustomTextField: React.FC<CustomTextFieldProps> = ({
+    label,
+    value,
+    onChange,
+    type = 'text',
+    placeholder = '',
+    fullWidth = true,
+    disabled = false,
+    error = false,
+    helperText = '',
+}) => {
+    return (
+        <TextField
+            label={label}
+            value={value}
+            onChange={onChange}
+            type={type}
+            placeholder={placeholder}
+            fullWidth={fullWidth}
+            disabled={disabled}
+            error={error}
+            helperText={helperText}
+            variant="outlined"
+        />
+    );
+};
