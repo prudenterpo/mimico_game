2ac272576e68c640c4f4ed304bbb86f021f05e14
rodrigopdo <rodrigo.prudente82@gmail.com>
2025-03-17 08:46:17 -0300

    Feat: Some settings in page.tsx.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
 import { Container, Typography } from '@mui/material';
 import { CustomCard } from '@/components/UI/CustomCard';
 
 export default function HomePage() {
-  return (
+
+    return (
       <Container maxWidth="sm">
-        <Typography variant="h3" gutterBottom>
-          Teste do CustomCard
-        </Typography>
-        <CustomCard title="Sala de Jogo" content="Clique para entrar nesta sala!" />
-        <CustomCard title="Histórico" content="Últimos jogos disputados." />
+          <Typography variant="h3" gutterBottom>Teste do CustomCard</Typography>
+          <CustomCard title="Sala de Jogo" content="Clique para entrar nesta sala!" />
+          <CustomCard title="Histórico" content="Últimos jogos disputados." />
       </Container>
   );
 }
